307f79b900a5e3c3f3dd930bf889509143810954
Darren Benson <darren.benson@deskpoint.com>
2020-08-19 14:49:50 +0100

    Refactor, Sprites & Vertical movement

--- hedgehogger.js
+++ hedgehogger.js
@@ -1,85 +1,169 @@
 function HedgehoggerGame(){
     const canvasWidth = 1000;
     const canvasHeight = 500;
-    const spriteWidth = 50;
-    const spriteHeight = 50;
-    var midXCanvas = Math.floor((canvasWidth-spriteHeight) / 2);
-    var midYCanvas = Math.floor((canvasHeight-spriteHeight) / 2);
+    const minSpeed = 2;
+    const maxSpeed = 10;
     var gameCanvas = document.getElementById("gameCanvas");
     var gameScreen = gameCanvas.getContext("2d");
     var playerSpeed = 2;
-    var gameWin = false;
+    var isRightArrowPressed = false;
+    var isLeftArrowPressed = false;
+    var isUpArrowPressed = false;
+    var isDownArrowPressed = false;
+    var horizontalSpeed = 0;
+    var verticalSpeed = 0;
     var gameOver = false;
 
-    class GameCharacter{
-        constructor(x, y, width, height, color, speed){
+    class GameSprite{
+        constructor(x, y, scrollSpeed, spriteUrl, width, height, scrollDirection){
             this.x = x;
             this.y = y;
+            this.scrollSpeed = scrollSpeed;
+            if (spriteUrl != null){
+                this.sprite = new Image();
+                this.sprite.src = spriteUrl;
+                this.sprite.width = width;
+                this.sprite.height = height;
+            }
             this.width = width;
             this.height = height;
-            this.color = color;
-            this.speed = speed;
+            this.scrollDirection = scrollDirection;
         }
 
-         HasCollided(gameCharacter){
-             return this.x < gameCharacter.x + gameCharacter.width 
-                    && this.x + this.width > gameCharacter.x
-                    && this.y < gameCharacter.y + gameCharacter.height
-                    && this.y + this.height > gameCharacter.y; 
+        HasCollided(gameSprite){
+             return this.x < gameSprite.x + gameSprite.width 
+                    && this.x + this.width > gameSprite.x
+                    && this.y < gameSprite.y + gameSprite.height
+                    && this.y + this.height > gameSprite.y; 
          }
 
-        MoveHorizontal(){            
-            if (this.x < spriteWidth){
-                this.x = spriteWidth;
-            } else if (this.x > canvasWidth - spriteWidth * 2){
-                this.x = canvasWidth - spriteWidth * 2
+        MoveHorizontal(amount){            
+            if (this.x < this.width){
+                this.x = this.width;
+            } else if (this.x > canvasWidth - this.width){
+                this.x = canvasWidth - this.width;
             }
             else{
-                this.x += this.speed;
+                this.x += amount;
             }
         }
 
-        MoveVertical(){
-            if (this.y < spriteHeight || this.y > canvasHeight - spriteHeight * 2){
-                this.speed = -this.speed;
+        MoveVertical(amount){
+            if (this.y < this.height){
+                this.y = this.height;
+            } else if (this.y > canvasHeight - this.height){
+                this.x = canvasHeight - this.height;
             }
-            this.y += this.speed;
+            else{
+                this.y += amount;
+            }
+        }
+
+        Scroll(){
+            if (this.scrollDirection == "Up"){
+                if (this.y <= canvasHeight - this.height){
+                    this.y += this.scrollSpeed;
+                } else{
+                    this.y = 0;
+                    this.scrollSpeed = GetRandomValue(minSpeed, maxSpeed);
+                }    
+            } else {
+                if (this.y >= 0){
+                    this.y -= this.scrollSpeed;
+                } else{
+                    this.y = canvasHeight - this.height;
+                    this.scrollSpeed = GetRandomValue(minSpeed, maxSpeed);
+                }    
+            }
         }
 
         Draw(){
-            gameScreen.fillStyle = this.color;
-            gameScreen.fillRect(this.x, this.y, this.width, this.height);    
+            if (this.sprite != null){
+                gameScreen.drawImage(this.sprite, this.x, this.y, this.width, this.height);
+            } else {
+                gameScreen.fillStyle = "rgb(0, 0, 0)";
+                gameScreen.fillRect(this.x, this.y, this.width, this.height);    
+            }
         }
     }
 
-    var player = new GameCharacter(spriteWidth, midYCanvas, spriteWidth, spriteHeight, "rgb(139,69,19)", 0);
-    var obsticles = [
-        new GameCharacter(spriteWidth * 3, spriteHeight, spriteWidth, spriteHeight, "rgb(0,0,255)", 2),
-        new GameCharacter(midXCanvas, midYCanvas, spriteWidth, spriteHeight, "rgb(0,0,255)", 3),
-        new GameCharacter(canvasWidth - spriteWidth * 4, spriteHeight, spriteWidth, spriteHeight, "rgb(0,0,255)", 4)        
-    ];
-    var goal = new GameCharacter(canvasWidth - spriteWidth * 2, midYCanvas, spriteWidth, spriteHeight, "rgb(255,255,0)", 0)
+    function GetRandomValue(minValue, maxValue){
+        let returnValue = Math.ceil(Math.random() * maxValue);
+        if (returnValue < minValue){
+            return minValue;
+        } else {
+            return returnValue;
+        }
+    }
 
+    var player = new GameSprite(0, Math.floor((canvasHeight-32) / 2), 0, "images/hedgehog.png", 32, 32);
+    var obsticles = [
+        new GameSprite(120, GetRandomValue(0, canvasHeight), GetRandomValue(minSpeed, maxSpeed), "images/RedCar.png", 32, 64, "Down"),
+        new GameSprite(240, GetRandomValue(0, canvasHeight), GetRandomValue(minSpeed, maxSpeed), "images/BlueCar.png", 32, 64, "Down"),
+        new GameSprite(360, GetRandomValue(0, canvasHeight), GetRandomValue(minSpeed, maxSpeed), "images/RedCar.png", 32, 64, "Down"),
+        new GameSprite(480, GetRandomValue(0, canvasHeight), GetRandomValue(minSpeed, maxSpeed), "images/RedStripedCar.png", 32, 64, "Up"),
+        new GameSprite(600, GetRandomValue(0, canvasHeight), GetRandomValue(minSpeed, maxSpeed), "images/RedCar.png", 32, 64, "Up"),
+        new GameSprite(720, GetRandomValue(0, canvasHeight), GetRandomValue(minSpeed, maxSpeed), "images/BlueCar.png", 32, 64, "Up"),
+        new GameSprite(840, GetRandomValue(0, canvasHeight), GetRandomValue(minSpeed, maxSpeed), "images/RedCar.png", 32, 64, "Up"),
+    ];
+    var goal = new GameSprite(canvasWidth - 32, player.y, 0, "images/Goal.png", 32, 32);
+    
     document.onkeydown = function(event){
         switch (event.code){
             case "ArrowRight":
-                player.speed = playerSpeed;
+                isRightArrowPressed = true;
+                horizontalSpeed = playerSpeed;
                 break;
             case "ArrowLeft":
-                player.speed = -playerSpeed;
+                isLeftArrowPressed = true;
+                horizontalSpeed = -playerSpeed;
                 break;
-        }
+            case "ArrowUp":
+                isUpArrowPressed = true;
+                verticalSpeed = -playerSpeed;
+                break;
+            case "ArrowDown":
+                isDownArrowPressed = true;
+                verticalSpeed = playerSpeed;
+                break;
+            }
     }
 
     document.onkeyup = function(event){
-        player.speed = 0;
-    }
-
-    function HasCollided(object1, object2){
-        //let xOverlap = Math.abs(object1.x - object2.x) <= Math.max(object1.width, object2.width);
-        //let yOverlap = Math.abs(object1.y - object2.y) <= Math.max(object1.height, object2.height);
-        //return xOverlap && yOverlap;
-        
+        switch (event.code){
+            case "ArrowRight":
+                isRightArrowPressed = false;
+                if (isLeftArrowPressed){
+                    horizontalSpeed = -playerSpeed;
+                } else {
+                    horizontalSpeed = 0;
+                }
+                break;
+            case "ArrowLeft":
+                isLeftArrowPressed = false;
+                if (isRightArrowPressed){
+                    horizontalSpeed = playerSpeed;
+                } else {
+                    horizontalSpeed = 0;
+                }
+                break;
+            case "ArrowUp":
+                isUpArrowPressed = false;
+                if (isDownArrowPressed){
+                    verticalSpeed = playerSpeed;
+                } else {
+                    verticalSpeed = 0;
+                }
+                break;
+            case "ArrowDown":
+                isDownArrowPressed = false;
+                if (isUpArrowPressed){
+                    verticalSpeed = +playerSpeed;
+                } else {
+                    verticalSpeed = 0;
+                }
+                break;
+        }
     }
 
     function DrawScreen(){
@@ -92,21 +176,21 @@ function HedgehoggerGame(){
     }
 
     function Update(){
-        player.MoveHorizontal();
+        player.MoveHorizontal(horizontalSpeed);
+        player.MoveVertical(verticalSpeed);
         obsticles.forEach(function(item){
             if (item.HasCollided(player)){
-                gameOver = true;
-                gameWin - false;
+                GameOver(false);
             }
-            item.MoveVertical();
+            item.Scroll();
         });
-        if (goal.HasCollided(player)){
-            gameOver = true;
-            gameWin = true;
+        if (goal.HasCollided(player)){            
+            GameOver(true);
         }
     }
 
-    function GameOver(){
+    function GameOver(gameWin){
+        gameOver = true;
         if (gameWin){
             alert("You Win!");
         } else {
@@ -120,9 +204,7 @@ function HedgehoggerGame(){
         Update();
         if (!gameOver){
             window.requestAnimationFrame(Step);
-        } else {
-            GameOver();
-        }
+        } 
     }
 
     Step();
